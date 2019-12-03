ec518fef152f9e1bf73dcb7eea3a28701b20475e
Walter <walterandres1259@gmail.com>
2019-12-03 12:04:08 -0500

    server listens

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+
+const db = require("./models/index")
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+//view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+//static assets
+app.use(express.static("public"));
+
+// routes
+
+//sync schema?
+db.sequelize.sync({ force: process.env.NODE_ENV !== "production" })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`==> Server listening at http://localhost:${PORT}/`);
+    });
+  });
