a46677638692796b2e11c0ea41c1396e39691910
Walter <walterandres1259@gmail.com>
2019-12-04 15:53:26 -0500

    can input to wishes table but the table needs to be made properly for the input

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("../models/index")
+
 router.get("/list", (req, res) => {
     res.render("data/list");
+});
+
+
+
+
+
+router.post("/list", (req, res) => {
+    db.Wish.create({
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        description: req.body.description
+    }).then(wish => {
+        console.log(wish);
+        // res.redirect("/list");
+    }).catch(err => {
+        console.log(err)
+    })
 });
 
 
